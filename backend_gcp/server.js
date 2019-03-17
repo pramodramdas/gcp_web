69a053ac74c69d6227cc9fdd10e89427d2d98e56
pramodramdas <pram25591@gmail.com>
2019-03-17 20:23:43 +0530

    gcp config

--- backend_gcp/server.js
+++ backend_gcp/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/api/getMessage', (req, res) => {
-	res.send('hi there hello');
+	res.send('hi there hello1');
 });
 
 app.listen(port, () => {
