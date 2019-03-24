6e65893d01b9a45a4de345da3ce5519ff030a485
pramodramdas <pram25591@gmail.com>
2019-03-24 11:59:01 +0530

    update deploument

--- backend_gcp/server.js
+++ backend_gcp/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/api/getMessage', (req, res) => {
-	res.send('hi there hello1');
+	res.send('hi there hello2');
 });
 
 app.listen(port, () => {
